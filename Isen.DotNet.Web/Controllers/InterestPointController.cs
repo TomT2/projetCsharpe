6b6de780af37716cd3656a0837fe93b99a167a01
cpituello <cedric.pituello@isen.yncrea.fr>
2018-05-12 12:19:13 +0200

    filter with multiple choice for each fields

--- Isen.DotNet.Web/Controllers/InterestPointController.cs
+++ Isen.DotNet.Web/Controllers/InterestPointController.cs
@@ -60,13 +60,40 @@ namespace Isen.DotNet.Web.Controllers
         [Route("api/[controller]/City/{city?}/Department/{department?}/Category/{category?}")]
         public virtual JsonResult Get(string city="all", string department="all", string category="all")
         {
+            string[] cities = city.Split(',');
+            string[] departments = department.Split(',');
+            string[] categories = category.Split(',');
+
             var filter = _repository
                 .GetAll()
-                .Where(i => (i.Address?.City?.Name.ToLower() == city.ToLower() || city.ToLower() == "all")&&
-                            (i.Address?.City?.Department.Name.ToLower() == department.ToLower() || department.ToLower() == "all")&&
-                            (i.Category?.Name?.ToLower() == category.ToLower() || category.ToLower() == "all"))
+                .Where(i => {
+                    bool city_filter = false; 
+                    bool department_filter = false;
+                    bool category_filter = false;
+
+                    foreach(string c in cities){
+                        if(i.Address?.City?.Name.ToLower() == c.ToLower() || c.ToLower() == "all"){
+                            city_filter = true;
+                        }
+                    }
+
+                    foreach(string d in departments){
+                        if(i.Address?.City?.Department.Name.ToLower() == d.ToLower() || d.ToLower() == "all"){
+                            department_filter = true;
+                        }
+                    }
+
+                    foreach(string ca in categories){
+                        if(i.Category?.Name?.ToLower() == ca.ToLower() || ca.ToLower() == "all"){
+                           category_filter = true;
+                        }
+                    }
+
+                    return city_filter && department_filter && category_filter;   
+                })
                 .Select(i => i.ToDynamic())
                 .ToList();
+
             return Json(filter);
         }
         
